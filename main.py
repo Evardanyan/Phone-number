c6dc5c587567d3a1b12092168a19f2545b2a2225
Evardanyan <64473803+Evardanyan@users.noreply.github.com>
2021-09-10 10:11:15 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import re
 
+string = input()
+# your code here
+string_regex = re.match(r'^\+(\d)[\s-]?(\d{3})[\s-]?(\d{3}[\s-]?\d{2}[\s-]?\d{2})$', string)
+
+if string_regex:
+    print(f"Full number:  {string_regex[0]} ")
+    print(f"Country code:  {string_regex[1]} ")
+    print(f"Area code:  {string_regex[2]} ")
+    print(f"Number:  {string_regex[3]} ")
+else:
+    print("No match")
